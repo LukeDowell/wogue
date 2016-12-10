877b11484e8ec8b18db8f6bb205369f410aa8981
Luke Dowell <Luke Dowell>
2016-12-10 11:14:07 -0600

    Checkin out netty

---
+++ wogue-client/src/main/kotlin/org/badgrades/wogue/gui/ClientFrame.kt
@@ -0,0 +1,10 @@
+package org.badgrades.wogue.gui
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+class ClientFrame : Application() {
+    override fun start(primaryStage: Stage?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
