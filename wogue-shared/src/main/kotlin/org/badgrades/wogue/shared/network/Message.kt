194956f530e83aae663adbe1fb3f5dd47e4bb99f
Luke Dowell <Luke Dowell>
2016-12-23 13:00:27 -0600

    Adds jackson annotations, includes type information on class

--- wogue-shared/src/main/kotlin/org/badgrades/wogue/shared/network/Message.kt
+++ wogue-shared/src/main/kotlin/org/badgrades/wogue/shared/network/Message.kt
@@ -1,9 +1,13 @@
 package org.badgrades.wogue.shared.network
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
 /**
  * All exchanges between the server and client are instances of this class
  */
 class Message(
         val event: Event,
+        
+        @JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="class")
         val payload: Any
 )
